009a190312f04ab3638ca21a3cd74302f87e9fa8
dbbaskette <dbbaskette@gmail.com>
2015-03-09 12:30:16 -0700

    dbb fixes

--- wait-for-slaves.sh
+++ wait-for-slaves.sh
@@ -7,16 +7,16 @@ number_of_slaves=`echo $(($number_of_slaves + 1))`
 `curl -i -u admin:admin http://$master_node_internal_dns:8080/api/v1/hosts > ambari-hosts.txt`
 
 number_of_registered_slaves=`grep -c "host_name" ambari-hosts.txt`
-echo "Number of slaves requested : " $number_of_slaves
-echo "Number of registred slaves found : " $number_of_registered_slaves
+echo "Number of slaves requested : " $number_of_slaves  >> phd.log
+echo "Number of registred slaves found : " $number_of_registered_slaves >> phd.log
 while [ "$number_of_registered_slaves" != "$number_of_slaves" ]
         do
         sleep 30
         echo "Trying again after sleep"
         `curl -i -u admin:admin http://$master_node_internal_dns:8080/api/v1/hosts > ambari-hosts.txt`
         number_of_registered_slaves=`grep -c "host_name" ambari-hosts.txt`
-        echo "Number of slaves requested : " $number_of_slaves
-        echo "Number of registred slaves found : " $number_of_registered_slaves
+        echo "Number of slaves requested : " $number_of_slaves >> phd.log
+        echo "Number of registred slaves found : " $number_of_registered_slaves >> phd.log
 done
 echo "Registered slave nodes match Requested slave nodes"
 exit 0
