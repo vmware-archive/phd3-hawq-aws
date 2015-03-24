281b6f39adbfd98b45e6eac918a53fa4cb2dc69b
dbbaskette <@Vengers12>
2015-03-24 02:20:24 -0400

    Lots o Changes

--- SetRepos.py
+++ SetRepos.py
@@ -1,43 +1,24 @@
 __author__ = 'root'
 
-import os
 import socket
+import json
 
 from requests.auth import HTTPBasicAuth
 import requests
 
-
 def getRepos():
     print "getRepo"
     # look through repo files and then submit via REST
-    repos = os.listdir("/etc/yum.repos.d")
     hostName = socket.getfqdn()
-    headers = {'X-Requested-By': 'Heffalump'}
+    headers = {"X-Requested-By": "Heffalump"}
     auth = HTTPBasicAuth('admin', 'admin')
-    # url = "http://" + hostName + ":8080/api/v1/stacks/PHD/versions/3.0/operating_systems/redhat6/repositories/PHD-3.0"
-
-    for repoFile in repos:
-        repo = repoFile[:-5]
-
-        if "PHD-3.0" in repo:
-            url = "http://" + hostName + ":8080/api/v1/stacks/PHD/versions/3.0/operating_systems/redhat6/repositories/" + repo
-            payload = {"Repositories": {"base_url": hostName + "/" + repo + "/"}}
-            print payload
-            print url
-            print requests.put(url, auth=auth, headers=headers, data=payload)
-        elif "PHD-UTILS" in repo:
-            url = "http://" + hostName + ":8080/api/v1/stacks/PHD/versions/3.0/operating_systems/redhat6/repositories/" + repo
-            payload = {"Repositories": {"base_url": hostName + "/" + repo + "/"}}
-            print payload
-            print url
-            print requests.put(url, auth=auth, headers=headers, data=payload)
-        elif "PADS" in repo:
-            url = "http://" + hostName + ":8080/api/v1/stacks/PHD/versions/3.0/operating_systems/redhat6/repositories/" + repo
-            payload = {"Repositories": {"base_url": hostName + "/" + repo + "/"}}
-            print payload
-            print url
-            print requests.put(url, auth=auth, headers=headers, data=payload)
-
+    url = "http://" + hostName + ":8080/api/v1/stacks/PHD/versions/3.0/operating_systems/redhat6/repositories/"
+    repos = json.loads(requests.get(url, auth=auth).text)["items"]
+    for repo in repos:
+        url = str(repo["href"])
+        repoID = str(repo["Repositories"]["repo_id"])
+        payload = "{\"Repositories\": {\"base_url\": \"http://" + hostName + "/" + repoID + "/\" , \"verify_base_url\" :false}}"
+        test = requests.put(url, auth=auth, headers=headers, data=payload)
 
 if __name__ == '__main__':
     print "PHD3 Repo Setup"
