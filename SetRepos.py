28e87cb3d705fe1747a09da3e21b3158799c5fe9
dbbaskette <@Vengers12>
2015-03-23 23:39:29 -0400

    Lots o Changes

--- SetRepos.py
+++ SetRepos.py
@@ -20,19 +20,19 @@ def getRepos():
         repo = repoFile[:-5]
 
         if "PHD-3.0" in repo:
-            url = "http://" + hostName + ":8080/api/v1/stacks/PHD/versions/3.0/operating_systems/redhat6/repositories/PHD-3.0"
+            url = "http://" + hostName + ":8080/api/v1/stacks/PHD/versions/3.0/operating_systems/redhat6/repositories/" + repo
             payload = {"Repositories": {"base_url": hostName + "/" + repo + "/"}}
             print payload
             print url
             print requests.put(url, auth=auth, headers=headers, data=payload)
         elif "PHD-UTILS" in repo:
-            url = "http://" + hostName + ":8080/api/v1/stacks/PHD/versions/3.0/operating_systems/redhat6/repositories/PHD-UTILS"
+            url = "http://" + hostName + ":8080/api/v1/stacks/PHD/versions/3.0/operating_systems/redhat6/repositories/" + repo
             payload = {"Repositories": {"base_url": hostName + "/" + repo + "/"}}
             print payload
             print url
             print requests.put(url, auth=auth, headers=headers, data=payload)
         elif "PADS" in repo:
-            url = "http://" + hostName + ":8080/api/v1/stacks/PHD/versions/3.0/operating_systems/redhat6/repositories/PHD-UTILS"
+            url = "http://" + hostName + ":8080/api/v1/stacks/PHD/versions/3.0/operating_systems/redhat6/repositories/" + repo
             payload = {"Repositories": {"base_url": hostName + "/" + repo + "/"}}
             print payload
             print url
