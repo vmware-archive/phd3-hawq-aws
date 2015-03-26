4602882d21b709cb0d9dd0045e46e91b749ec0c8
dbbaskette <@Vengers12>
2015-03-26 04:12:16 -0400

    Blueprint Changes 001

--- AgentRegistration.py
+++ AgentRegistration.py
@@ -1,5 +1,26 @@
 __author__ = 'dbaskette'
 import argparse
+import socket
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def registrationMonitor(numAgents):
+    hostName = socket.getfqdn()
+    auth = HTTPBasicAuth('admin', 'admin')
+    url = "http://" + hostName + ":8080/api/v1/hosts"
+    complete = False
+    while not complete:
+        registeredCount = 0
+        agentInfo = requests.get(url, auth=auth)
+        ambariHosts = open("ambari-hosts.json", "w")
+        for line in agentInfo:
+            if line.contains("host_name"):
+                ambariHosts.write(line)
+                registeredCount += 1
+        if registeredCount == numAgents:
+            complete = True
 
 
 def cliParse():
@@ -7,8 +28,7 @@ def cliParse():
     parser = argparse.ArgumentParser(description='Agent Registration')
     subparsers = parser.add_subparsers(help='sub-command help', dest="subparser_name")
     parser_monitor = subparsers.add_parser("monitor", help="Monitor Agent Registration")
-    parser_monitor.add_argument("--ambari", dest='ambariServer', action="store", help="Ambari Server", required=True)
-    parser_monitor.add_argument("--agents", dest='anumAgents', action="store", help="Number of Agents to Register",
+    parser_monitor.add_argument("--agents", dest='numAgents', action="store", help="Number of Agents to Register",
                                 required=True)
 
     args = parser.parse_args()
@@ -18,3 +38,5 @@ def cliParse():
 if __name__ == '__main__':
     print "Agent Registration"
     args = cliParse()
+    numAgents = args.numAgents + 1  # Account for Agent on Ambari Server itself
+    registrationMonitor(numAgents)
